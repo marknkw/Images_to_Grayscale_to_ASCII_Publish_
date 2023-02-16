c8e82aa6727777abe2b6878de017d463dad80f50
marknkw <37668487+marknkw@users.noreply.github.com>
2023-02-15 21:12:32 -0300

    Update_Creates_ASCII_FOLDER
    15/02/2023 - Function creates ASCII folder

--- grayscale.py
+++ grayscale.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import os
 import math
+import time
+import sys
 
 
 def verificar_imagem(nome):
@@ -21,6 +23,29 @@ def verificar_diretorio(nome):
         return False
 
 
+def verificar_diretorio_ascii(nome):
+    diretorio_base = os.path.join(os.getcwd(), nome)
+    caminho = os.path.exists(diretorio_base)
+    print(diretorio_base)
+    if caminho:
+        return False
+
+    else:
+        try:
+            os.mkdir(diretorio_base)
+            return True
+        except FileExistsError:
+            return False
+
+
+def print_digitado(frase):
+    for i in frase:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+
 def verificar_tamanho_imagem(imagem):
     return (Image.open(imagem)).size
 
@@ -37,7 +62,7 @@ def salvar_imagem(imagem, tamanho, coordenadas_cinza):
     print(imagem_cinza_nova.size)
     imagem_cinza_nova.show()
     imagem_cinza_nova.save("Resources/grayscale.png")
-    exit()
+    return imagem_cinza_nova
 
 
 def media_dos_pixels(r, g, b, l_im, a_im):
@@ -45,7 +70,7 @@ def media_dos_pixels(r, g, b, l_im, a_im):
     coordenadas_cinza = []
     for i in range(0, l_im):
         for j in range(0, a_im):
-            coordenadas = x, y = i, j
+            coordenadas = i, j
             red, green, blue = float(r.getpixel(coordenadas)), float(g.getpixel(coordenadas)), float(b.getpixel(coordenadas))
             media_das_cores = 0.2989 * red + 0.5870 * green + 0.1140 * blue
             if (media_das_cores % 1) == 0:
@@ -87,13 +112,36 @@ def canaistamanho_imagem():
         print("Pasta não existe...")
         exit()
 
+"""def imagem_cinza_para_ascii(imagem):
+    
+    """
+escala_cinza_dois = "@%#*+=-:. "
+escala_cinza_um = r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`'. "
 
 r_out, g_out, b_out, l_im_out, a_im_out = canaistamanho_imagem()
 """r_out.show(), g_out.show(), b_out.show()"""
-print(l_im_out, a_im_out)
+"""print(l_im_out, a_im_out)"""
 coordenadas_globais = [int(l_im_out), int(a_im_out)]
 imagem_cinza_out, coordenadas_cinza_globais = media_dos_pixels(r_out, b_out, g_out, l_im_out, a_im_out)
-"""print(imagem_cinza)"""
-salvar_imagem(imagem_cinza_out, coordenadas_globais, coordenadas_cinza_globais)
+imagem_cinza_final = salvar_imagem(imagem_cinza_out, coordenadas_globais, coordenadas_cinza_globais)
 
-"""print(imagem_cinza)"""
+while True:
+    print_digitado("Digite o nome do diretório ASCII que deseja criar: ")
+    nome_diretorio_ascii = input()
+    if nome_diretorio_ascii.isalpha():
+        if nome_diretorio_ascii == "sair":
+            exit()
+        bool_diretorio = verificar_diretorio_ascii(nome_diretorio_ascii)
+        if bool_diretorio:
+            print_digitado("Diretório criado...\n")
+            break
+        else:
+            print_digitado("Diretório já existe...\n")
+            break
+    elif nome_diretorio_ascii.lower().isalpha() == "sair":
+        exit()
+    else:
+        print_digitado("O nome do diretório contém caracteres não permitidos...\n" +
+                       "Por favor, use somente letras de A até Z...\n" +
+                       "Caso queira sair, digite 'sair'...\n")
+
